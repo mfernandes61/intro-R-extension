8e31b43c889798532e616385f3add23d2ef4e10f
Mark Fernandes <mfernandes61@gmail.com>
2024-02-27 10:29:41 +0000

    Add files via upload
    Initial commit

---
+++ setup.R
@@ -0,0 +1,41 @@
+## file structure
+if (! file.exists("data")) dir.create("data")
+
+if (!file.exists("data/portal_data_joined.csv")) {
+    download.file("https://ndownloader.figshare.com/files/2292169",
+                  "data/portal_data_joined.csv")
+}
+
+## knitr options
+library(knitr)
+library(methods)
+suppressPackageStartupMessages(library(tidyverse))
+knitr::opts_chunk$set(results='hide', comment = "#>", purl = FALSE)
+
+### Custom hooks
+
+## hook for challenges answers
+
+knitr::knit_hooks$set(answer = function(before, options, envir) {
+    if (before) {
+        paste(
+            "<div class=\"accordion\">",
+              "<h3 class=\"toc-ignore\">Answer</h3>",
+              "<div style=\"background: #fff;\">",  sep = "\n")
+    } else {
+        paste("</div>", "</div>", sep = "\n")
+    }
+})
+
+eng_text_answer <- knitr:::eng_html_asset(
+                               paste(
+                                   "<div class=\"accordion\">",
+                                   "<h3 class=\"toc-ignore\">Answer</h3>",
+                                   "<div style=\"background: #fff;\">",
+                                   "<p>",  sep = "\n"),
+                               paste(
+                                   "</div>", "</div>", "</p>", sep = "\n"
+                               )
+                           )
+
+knitr::knit_engines$set(text_answer = eng_text_answer)
